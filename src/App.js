c01ce3bee40a83ae53457155be8ae2c5d7870559
Isaiah Murray <murray.isaiah13@gmail.com>
2021-12-14 12:55:29 -0500

    Changing spacing for welcome header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Auth from './components/Auth/Auth';
 import Display from './components/Display';
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  app: {
+    color: "snow"
+  },
+  welcome: {
+    margin: "0px",
+     marginBottom: "-20px",
+
+    //! Styling alterations for screen breakpoints
+    //* Mobile
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "0px",
+    },
+    //* Tablet/Desktop
+    [theme.breakpoints.up("md")]: {
+      
+    }
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
   const [sessionToken, setSessionToken] = useState(null); 
   const [loading, setLoading] = useState(false);
+
   useEffect(() => { 
     if(localStorage.getItem('token')){
       setSessionToken(localStorage.getItem('token'));
@@ -29,8 +52,8 @@ function App() {
     : <Auth updateToken={updateToken}/>)
   }
   return (
-    <div className="App">
-      <h1>Welcome to your Journal!</h1>
+    <div className={classes.app}>
+      <h1 className={classes.welcome}>Welcome to your Journal!</h1>
      {protectedViews()}
     </div>
   );
