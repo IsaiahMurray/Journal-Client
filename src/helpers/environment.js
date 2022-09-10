40b55d31dcfa1431d74a7a5759ff8e167d8f487b
Isaiah Murray <murray.isaiah13@gmail.com>
2022-09-10 16:29:59 -0400

    Changed helper url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,19 +1,19 @@
-// let APIURL = 'https://arcane-tor-58586.herokuapp.com/https://ism-journal-server.herokuapp.com';
-let APIURL; 
+ let APIURL = 'https://arcane-tor-58586.herokuapp.com/https://ism-journal-server.herokuapp.com';
+// let APIURL; 
 
-switch(window.location.hostname){
-    case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:4000';
-        break;
+// switch(window.location.hostname){
+//     case 'localhost' || '127.0.0.1':
+//         APIURL = 'http://localhost:4000';
+//         break;
 
-    case 'ism-journal-server.herokuapp.com':
-        APIURL = 'https://arcane-tor-58586.herokuapp.com/https://ism-journal-server.herokuapp.com'
-    break;
+//     case 'ism-journal-server.herokuapp.com':
+//         APIURL = 'https://arcane-tor-58586.herokuapp.com/https://ism-journal-server.herokuapp.com'
+//     break;
 
-    default:
-        APIURL = 'https://arcane-tor-58586.herokuapp.com/https://ism-journal-server.herokuapp.com'
-    break;
+//     default:
+//         APIURL = 'https://arcane-tor-58586.herokuapp.com/https://ism-journal-server.herokuapp.com'
+//     break;
 
-}
+// }
 
 export default APIURL;
